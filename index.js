acc78aa87c9e98c9b86a48c6121a0235454569a8
Penelope Drago <prd@drago.com.au>
2023-04-06 18:01:05 +1000

    add code to change DOM attempt 2

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Write your code here!
+
+// Remove DOM node 'main#main'
+  main.remove();
+
+
+// Add newHeader variable
+  const newHeader = document.createElement('h1');
+  newHeader.setAttribute('id', 'victory');
+  newHeader.textContent = "Penny is the champion";
+  document.body.append(newHeader);
